8137a67acd09c4ab7f75b8c54928207c62f3b84a
Nikita <nikita.ivanov@1win.pro>
2025-02-08 22:53:43 +0500

    fix

--- devtools.js
+++ devtools.js
@@ -1,9 +1,1 @@
-document.getElementById("submitBtn").addEventListener("click", () => {
-  const input1 = document.getElementById("input1").value;
-  const input2 = document.getElementById("input2").value;
-  const input3 = document.getElementById("input3").value;
 
-  console.log("Input 1:", input1);
-  console.log("Input 2:", input2);
-  console.log("Input 3:", input3);
-});
